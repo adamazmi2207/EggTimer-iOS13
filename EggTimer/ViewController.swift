055368fab136f3c33ebe266ce6b523e03c896d28
adamazmi2207 <66166999+adamazmi2207@users.noreply.github.com>
2021-05-22 23:14:31 +0900

    initial commit
    TODO: add timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var count = 60
 
+    @IBOutlet weak var countDownLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        var timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(UIMenuController.update), userInfo: nil, repeats: true)
+    }
+
+    @objc func update() {
+        if(count > 0) {
+            print("\(count) seconds to the end of the world")
+            countDownLabel.text = String(count)
+            count -= 1
+//            countDownLabel.text = String(count--)
+        }
+    }
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness: String? = sender.currentTitle
+        
+       
+        print(eggTimes[hardness!]!)
+    }
 }
